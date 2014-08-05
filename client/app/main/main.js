28df4787f3e0c35220778cf8743d5fd1b3987091
Konstantin Stepanyuk <24const@gmail.com>
2014-08-05 21:04:10 +0700

    added generated project

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('suggestDishApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
